21492f43e851f91e04d1e3087e7b83af5a41bddb
bharambeakshay <bharambeakshay1997@gmail.com>
2021-03-01 23:46:01 +0530

    [Akshay] Refactor.  chanaged router parts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
-// Routers will be here
-const App = () =>
-    <div>
-        
 
-    </div>;
-export default App
+import React from 'react';
+import './App.scss';
+import { Router as Routing, Route, Switch } from 'react-router-dom'
+import Dashboard from './Components/Dashboard/Dashoard';
+import Login from './Components/Login/Login'
+import history from './Components/history'
+import Signup from "./Components/Signup/Signup"
+import Cart from "./Components/Cart/Cart"
+import Header from './Components/Header/Header';
+
+
+const App = (props) => {
+  return (
+    <Routing history={history}>
+   
+        <Switch>
+          <Route path="/" exact component={Dashboard}{...props}></Route>
+          <Route path="/header" component={Header}{...props}></Route>
+          <Route path="/login"  component={Login}{...props}></Route>
+          <Route path="/signup"  component={Signup}{...props}></Route>
+        <Route path="/cart" component={Cart}{...props}></Route>
+          
+
+        </Switch>
+  
+    </Routing >
+  );
+};
+
+export default App;
