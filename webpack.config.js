8adf9e166d5083f8803136585a0f6f9aa407fa2a
bharambeakshay <bharambeakshay1997@gmail.com>
2021-03-01 23:46:40 +0530

    [Akshay] Update. updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,86 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
+
+
+const port = process.env.PORT || 8080;
+
 module.exports = {
-    entry: './src/index.js',
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.[hash].js",
+        publicPath: '/'
+    },
+    devtool: "inline-source-map",
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/, // add this line in webpack.config.js
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
-            }
-        ]
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            localsConvention: "camelCase",
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]',
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+
+        ],
+    },
+    plugins: [
+        new Dotenv(),
+
+        new HtmlWebpackPlugin({
+            template: "dist/index.html",
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    devServer: {
+        host: "localhost",
+        port: port,
+        historyApiFallback: true,
+        open: true,
+        hot: true
     },
     resolve: {
-        extensions: ['*', '.js', '.jsx'] // add this line in  webpack.config.js
+        extensions: ["*", ".js", ".jsx", ".css", ".scss", ".png"],
+        alias: {
+            "react-dom": "@hot-loader/react-dom",
+        },
     },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: './dist'
-    }
+
 };
 
 
 
 
-// module.exports = {
-//     entry: './src/index.js',
-//     module: { //////////////
-//         // add this
-//         rules: [ // line
-//             { // of
-//                 test: /\.(js)$/, // code
-//                 exclude: /node_modules/, // in
-//                 use: ['babel-loader'] // webpack.config.js
-//             } // file
-//         ] //
-//     }, //
-//     resolve: { //
-//         extensions: ['*', '.js'] //
-//     }, //////////////
-//     output: {
-//         path: __dirname + '/dist',
-//         publicPath: '/',
-//         filename: 'bundle.js'
-//     },
-//     devServer: {
-//         contentBase: './dist'
-//     }
-// };
-
-
-
-
-
-
-// module.exports = {
-//     // 1
-//     entry: './src/index.js',
-//     // 2
-//     output: {
-//         path: __dirname + '/dist',
-//         publicPath: '/',
-//         filename: 'bundle.js'
-//     },
-//     // 3
-//     devServer: {
-//         contentBase: './dist'
-//     }
-// };
