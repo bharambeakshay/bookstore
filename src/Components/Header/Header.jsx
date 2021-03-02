91e365756348052020bfc781a389623dde6fce81
bharambeakshay <bharambeakshay1997@gmail.com>
2021-03-02 23:09:21 +0530

    [Akshay] Refactor . displaying books using context api

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -102,10 +102,12 @@ const Header = (props) => {
               </Card>
             </div>
           </li>
-
-          <Link to="/Cart">
+          {/* <Link to="/cart">Link
+            </Link> */}
+         
             <Nav.Link className=" text-center cart-nav-header">
               <span>
+              <Link to ="/Cart">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="25"
@@ -116,15 +118,16 @@ const Header = (props) => {
                   viewBox="0 0 16 16"
                 >
                   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                </svg>
+                </svg></Link>
               </span>
               <span className="badge badge-light cart-badge ">0</span>
 
-              <span>
-                <h2 className="header-cart-name">Cart</h2>
+              <span><Link to ="/Cart">
+                <h2 className="header-cart-name">Cart</h2></Link>
               </span>
             </Nav.Link>
-          </Link>
+         
+
         </Nav>
       </Navbar>
       {/*  Log in and signup Modal */}
@@ -132,10 +135,8 @@ const Header = (props) => {
         <Modal.Header>
           <Modal.Title className="login-upper-button">
             <button className="btn btn-light" onClick={handleLoginComp}>
-
               Login
             </button>
-            
           </Modal.Title>
           <Modal.Title className="signup-upper-button">
             <button className="btn btn-light" onClick={handleSignUpComp}>
@@ -144,7 +145,7 @@ const Header = (props) => {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body className="modal-login-body">
-          {isLoginOpen ? <Login props={props}/> : <SignUp props={props} />}
+          {isLoginOpen ? <Login props={props} /> : <SignUp props={props} />}
           {/* <div className="login-form-modal"><Login /></div> */}
           <div className="login-form-modal">{/* <SignUp /> */}</div>
           {/* <SignUp /> */}
