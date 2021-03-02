79c8e19aef9ebc20f064b2131b822f51d617dbd3
bharambeakshay <bharambeakshay1997@gmail.com>
2021-03-02 23:11:37 +0530

    [Akshay] Refactor. global display context is created

--- src/Components/Dashboard/Dashoard.jsx
+++ src/Components/Dashboard/Dashoard.jsx
@@ -1,24 +1,42 @@
 /** @format */
-
+import React, { useState, useEffect } from "react";
 // import Displaybook from "../Displaybook/Displaybook"
 import Header from "../Header/Header";
 import Displaybook from "../Displaybook/Displaybook";
+import BookService from "../../Services/BookService";
+import bookListContext from "../Context";
+import  "./dashboard.scss"
 
-import React, { Component } from "react";
+import Footer from "../Footer/Footer";
 
-export default class Dashoard extends Component {
-  constructor(props) {
-    super(props);
-    console.log("from dashboard", this.props);
-  }
+const DisplayBooks = (props) => {
+  const [BookList, setBookList] = useState([]);
+  useEffect(() => {
+    getBooks();
+  }, []);
+  const getBooks = () => {
+    BookService.getBooks()
+      .then((response) => {
+        console.log(response);
+        setBookList(response.data.result);
+        books.map((data) => (data.isCart = false));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-  render() {
-    return (
-      <div>
-        <Header props={this.props} />
-
+  return (
+    <React.Fragment>
+      <Header />
+      <div className="dashboraddisplay">
+      <bookListContext.Provider value={{ books: BookList }}>
         <Displaybook />
+      </bookListContext.Provider>
       </div>
-    );
-  }
-}
+      {/* <Footer /> */}
+    </React.Fragment>
+  );
+};
+
+export default DisplayBooks;
